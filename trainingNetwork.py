311326b3e8904bb8bb1f3ba49c78a3d26c06e10d
ejmejm <ninjaedan@aol.com>
2017-05-04 13:07:33 -0400

    Fixed forward prop

--- trainingNetwork.py
+++ trainingNetwork.py
@@ -14,32 +14,29 @@ x = tf.placeholder(tf.float32, [None, board_size * board_size])
 y = tf.placeholder(tf.float32, [None, 2])
 
 def nn_forward(data):
-    print("TEST1111111111")
     hidden_1_layer = {"weights": tf.Variable(tf.random_normal([board_size * board_size, n_nodes_hl1])),
     "biases": tf.Variable(tf.random_normal([n_nodes_hl1]))}
+    #print(tf.shape(hidden_1_layer["weights"]))
 
-    print("TEST2")
     hidden_2_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
     "biases": tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
     hidden_3_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
     "biases": tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
-    out_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+    output_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
     "biases": tf.Variable(tf.random_normal([n_classes]))}
 
-    print("TEST3")
     l1 = tf.add(tf.matmul(data, hidden_1_layer["weights"]), hidden_1_layer["biases"])
-    print("TEST4ghchgfhdfghfghfghfghfghh")
     l1 = tf.nn.relu(l1)
 
-    l2 = tf.add(tf.matmul(data, hidden_2_layer["weights"]), hidden_2_layer["biases"])
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer["weights"]), hidden_2_layer["biases"])
     l2 = tf.nn.relu(l2)
 
-    l3 = tf.add(tf.matmul(data, hidden_3_layer["weights"]), hidden_3_layer["biases"])
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer["weights"]), hidden_3_layer["biases"])
     l3 = tf.nn.relu(l3)
 
-    output = tf.matmul(data, output_layer["weights"]) + output_layer["biases"]
+    output = tf.matmul(l3, output_layer["weights"]) + output_layer["biases"]
 
     return output
 
@@ -55,8 +52,8 @@ def train_neural_network(x, gameData):
 
         for epoch in range(hm_epochs):
             epoch_loss = 0
-            for index in range(int(len(gameData/batch_size))):
-                board = np.zeroes(board_size * board_size)
+            for index in range(int(len(gameData)/batch_size)):
+                board = np.zeros(board_size * board_size)
                 for node in gameData[batch_size * epoch + index].get_main_sequence():
                     board = -board; # Changes player perspective, black becomes white and vice versa
                     if node.get_move()[1] != None:
